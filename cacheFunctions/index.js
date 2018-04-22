f0ee3932f7d58f2a9b2d63da7179f667e35697cb
Naval Patel <navalpatel384@outlook.com>
2018-04-21 19:40:32 -0700

    updated cacheFunctions

---
+++ cacheFunctions/index.js
@@ -0,0 +1,36 @@
+const AWS = require('aws-sdk');
+const https = require('https');
+const rp = require('request-promise');
+const s3 = new AWS.S3();
+
+
+function writeToS3(bucket, key, data){
+    var s3 = new AWS.S3();
+        var params = {
+            Bucket : bucket,
+            Key : key,
+            Body : data
+        }
+        s3.putObject(params, function(err, data) {
+          if (err) {
+              console.log(err, err.stack);
+          }
+          else {
+              console.log(data);
+          }
+        });
+}
+
+exports.handler = function(event, context, callback) {
+      rp({
+        uri: `https://hackerone.com/programs/search.json?query=type%3Ahackerone&limit=200`,
+        transform: function (body) {
+          return body;
+        }
+      }).then((data) => {
+        writeToS3('bugbrowserdev', 'hackerone.json', data);
+        callback(null); //callback(response.StatusCode);
+      }).catch((error) => {
+        console.log(error);
+      });
+}
