00f3d64292af9d85604bdacfe88a281174fcc6b8
Avi Shah <as0218@pleasantonusd.net>
2018-04-21 21:22:42 -0700

    Fixed caching function and added AWS ACL
    Made file public and fixed function with IAM policies.

--- cacheFunctions/index.js
+++ cacheFunctions/index.js
@@ -3,22 +3,23 @@ const https = require('https');
 const rp = require('request-promise');
 const s3 = new AWS.S3();
 
-
 function writeToS3(bucket, key, data){
-    var s3 = new AWS.S3();
-        var params = {
-            Bucket : bucket,
-            Key : key,
-            Body : data
-        }
-        s3.putObject(params, function(err, data) {
-          if (err) {
-              console.log(err, err.stack);
-          }
-          else {
-              console.log(data);
-          }
-        });
+  AWS.config.region = 'us-east-1';
+  var s3 = new AWS.S3();
+  var params = {
+      Bucket : bucket,
+      Key : key,
+      Body : data,
+      ACL:'public-read'
+  }
+  s3.putObject(params, function(err, data) {
+    if (err) {
+        console.log(err, err.stack);
+    }
+    else {
+        console.log(data);
+    }
+  });
 }
 
 exports.handler = function(event, context, callback) {
